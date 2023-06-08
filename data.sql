be43c9eccd19c34c1d54950874e83d9565a9a782
Fredo St Fleur <fredostfleur23@gmail.com>
2023-06-07 21:19:11 -0400

    fix insert values

--- data.sql
+++ data.sql
@@ -20,19 +20,19 @@ INSERT INTO public.animals(
 
 INSERT INTO public.animals(
 	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-	VALUES (5, 'Charmander', '2020-02-08', 0, 'false', 11);
+	VALUES (5, 'Charmander', '2020-02-08', 0, 'false', -11);
 
 	INSERT INTO public.animals(
 	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-	VALUES (6, 'Plantmon', '2021-11-15', 2, 'true', 5.7);
+	VALUES (6, 'Plantmon', '2021-11-15', 2, 'true', -5.7);
 
 	INSERT INTO public.animals(
 	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-	VALUES (7, 'Squirtle', '1993-04-02', 3, 'false', 12.13);
+	VALUES (7, 'Squirtle', '1993-04-02', 3, 'false', -12.13);
 
 	INSERT INTO public.animals(
 	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-	VALUES (8, 'Angemon', '2005-05-12', 1, 'true', 45);
+	VALUES (8, 'Angemon', '2005-05-12', 1, 'true', -45);
 
 	INSERT INTO public.animals(
 	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
