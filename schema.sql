7d2a8a07628be43f2610c9e773eb1ad3837e92b5
Fredo St Fleur <fredostfleur23@gmail.com>
2023-06-10 12:03:46 -0400

    Create vets ,specializations and visits table

--- schema.sql
+++ schema.sql
@@ -37,3 +37,32 @@ CREATE TABLE species (
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD species_id BIGINT REFERENCES species (id);
 ALTER TABLE animals ADD owner_id BIGINT REFERENCES owners (id);
+
+
+
+/* last Milestone  */
+
+CREATE TABLE vets (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	age INT NOT NULL,
+	date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+	species_id BIGINT REFERENCES species (id),
+	vets_id BIGINT REFERENCES vets (id)
+);
+
+CREATE TABLE visits (
+	animals_id BIGINT REFERENCES animals (id),
+	vets_id BIGINT REFERENCES vets (id),
+	date_of_visit DATE
+);
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+CREATE INDEX animals_id_asc on visits (animals_id ASC);
+CREATE INDEX vets_id_asc on visits (vets_id);
+CREATE INDEX email_id on owners(email ASC); 
